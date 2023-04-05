e86a54a682943ddb3c5f5d84e88f011d0ab77c95
1223v <1223v@naver.com>
2023-04-05 22:16:13 +0900

    fix: 강의 생성 페이지 수정

--- src/components/views/LectureAddPage/LectureAddPage.jsx
+++ src/components/views/LectureAddPage/LectureAddPage.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import NavBar from "../NavBar/NavBar";
 import LectureListPage from "./Sections/LectureListPage";
+import StudentAddModal from "../StudentAddModal/StudentAddModal";
 
 function LectureAddPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClickButton = () => {
+    setIsOpen(true);
+  };
+
   return (
     <LectureBackgroudDiv>
       <NavBar />
@@ -79,56 +86,85 @@ function LectureAddPage() {
           </LectureNameinputDiv>
         </LectureNameDiv>
         <hr style={{ background: "#d3d3d3", height: "1px", border: "0" }} />
-        <div>
-          <div>개설전공</div>
-          <div>
-            <select class="inputStyle01" name="lecture_major">
+        <LectureNameDiv>
+          <LectureName>개설 전공</LectureName>
+          <LectureNameinputDiv style={{ marginTop: "10px" }}>
+            <select
+              id="countries"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+            >
+              <option selected>전공</option>
               <option value="한일번역">한일번역</option>
-              <option>한일통역</option>
-              <option>한중번역</option>
-              <option>한중통역</option>
-              <option>한영번역</option>
-              <option>한영통역</option>
-              <option>한불번역</option>
-              <option>한불통역</option>
+              <option value="한중번역">한중번역</option>
+              <option value="한러번역">한러번역</option>
+              <option value="한불번역">한불번역</option>
+              <option value="한일통역">한일통역</option>
+              <option value="한중통역">한중통역</option>
+              <option value="한러통역">한러통역</option>
+              <option value="한불통역">한불통역</option>
             </select>
-          </div>
-        </div>
-        <div>
-          <div>분반</div>
-          <div>
-            <select class="inputStyle01" name="lecture_separated">
-              <option value="1분반">1분반</option>
-              <option>2분반</option>
-              <option>3분반</option>
-              <option>4분반</option>
-              <option>5분반</option>
-              <option>6분반</option>
-              <option>7분반</option>
-              <option>8분반</option>
-              <option>9분반</option>
-              <option>10분반</option>
+          </LectureNameinputDiv>
+        </LectureNameDiv>
+        <hr style={{ background: "#d3d3d3", height: "1px", border: "0" }} />
+        <LectureNameDiv>
+          <LectureName>학습 분반</LectureName>
+          <LectureNameinputDiv style={{ marginTop: "10px" }}>
+            <select
+              id="countries"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+            >
+              <option selected>분반</option>
+              <option value="1분반">1 분반</option>
+              <option value="2분반">2 분반</option>
+              <option value="3분반">3 분반</option>
+              <option value="4분반">4 분반</option>
+              <option value="5분반">5 분반</option>
+              <option value="6분반">6 분반</option>
+              <option value="7분반">7 분반</option>
+              <option value="8분반">8 분반</option>
+              <option value="9분반">9 분반</option>
+              <option value="10분반">10 분반</option>
             </select>
-          </div>
-        </div>
-        <div>
-          <div>담당교수</div>
-          <div>
-            <input
+          </LectureNameinputDiv>
+        </LectureNameDiv>
+        <hr style={{ background: "#d3d3d3", height: "1px", border: "0" }} />
+        <LectureNameDiv>
+          <LectureName>담당교수</LectureName>
+          <LectureNameinputDiv>
+            <LectureNameinput
               type="text"
-              class="inputStyle01"
-              name="lecture_professor"
-              value="교수"
+              placeholder="교수"
+              size="10"
+              maxlength="8"
             />
-          </div>
-        </div>
+          </LectureNameinputDiv>
+        </LectureNameDiv>
+        <hr style={{ background: "#d3d3d3", height: "1px", border: "0" }} />
         <div>
-          <div>수강생 명단</div>
+          <LectureNameDiv>
+            <LectureName>수강생 명단</LectureName>
+            <LectureName2>
+              <StudentAddButton onClick={onClickButton}>
+                + 추가
+              </StudentAddButton>
+              {isOpen && (
+                <StudentAddModal
+                  open={isOpen}
+                  onClose={() => {
+                    setIsOpen(false);
+                  }}
+                />
+              )}
+            </LectureName2>
+          </LectureNameDiv>
           <div>
             <LectureListPage />
           </div>
         </div>
       </LectureAddFormDiv>
+      <LectureCreateDiv>
+        <LectureCreateButton>생성하기</LectureCreateButton>
+      </LectureCreateDiv>
     </LectureBackgroudDiv>
   );
 }
@@ -138,7 +174,7 @@ export default LectureAddPage;
 const LectureBackgroudDiv = styled.div`
   background-color: #f7f7fa;
   width: 100%;
-  height: 900px;
+  height: 1500px;
 `;
 
 const LectureAddFormDiv = styled.div`
@@ -147,6 +183,7 @@ const LectureAddFormDiv = styled.div`
   margin: auto;
   background-color: #ffffff;
   width: 800px;
+  height: 80%;
   @media screen and (max-width: 768px) {
     width: 320px;
   }
@@ -186,6 +223,12 @@ const LectureNameDiv = styled.div`
   }
 `;
 
+const LectureName2 = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex-grow: 2;
+`;
+
 const LectureNameinput = styled.input`
   width: 100%;
   height: 48px;
@@ -210,4 +253,43 @@ const LectureName = styled.div`
   margin: 20px;
   flexgrow: 1;
   fontweight: 500;
+`;
+
+const LectureCreateDiv = styled.div`
+  position: fixed;
+  bottom: 0px;
+  z-index: 2;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  width: 100%;
+  height: 4rem;
+  background: rgb(255, 255, 255);
+  box-shadow: rgb(232, 232, 238) 0px 1px 0px inset;
+`;
+
+const LectureCreateButton = styled.button`
+  height: 3rem;
+  font-size: 0.975rem;
+  font-weight: 800;
+  line-height: 1.375rem;
+  width: 100%;
+  border-radius: 0.5rem;
+  margin: 20px;
+  color: #fff;
+  background-color: #2e462f;
+  border-color: transparent;
+`;
+
+const StudentAddButton = styled.button`
+  height: 2.5rem;
+  font-size: 0.975rem;
+  font-weight: 800;
+  line-height: 1.375rem;
+
+  border-radius: 0.5rem;
+  margin: 12px 15px;
+  color: #fff;
+  background-color: #2e462f;
+  border-color: transparent;
 `;
