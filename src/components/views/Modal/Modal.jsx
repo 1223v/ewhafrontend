5a9a72b8a3b396512fc34d067a2f8f6766885359
1223v <1223v@naver.com>
2023-04-09 03:59:58 +0900

    fix: 강의 추가 페이지 ui 변경

--- src/components/views/Modal/Modal.jsx
+++ src/components/views/Modal/Modal.jsx
@@ -28,7 +28,7 @@ function Modal({ onClose, weeklist }) {
           </CloseButton>
 
           <Contents>{weeklist[0].week}주차 과제</Contents>
-          <ModalContent />
+          <ModalContent weeklist={weeklist} />
         </ModalWrap>
       </Overlay>
     </ModalContainer>
