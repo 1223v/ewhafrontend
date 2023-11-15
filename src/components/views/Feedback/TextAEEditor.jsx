00fd020ea8e1bfed437c3bd3d7514531136c7665
1223v <1223v@naver.com>
2023-11-16 01:49:24 +0900

    Fix: 실시간 TextAEEditor의 변경 감지 이벤트 키 수정

--- src/components/views/Feedback/TextAEEditor.jsx
+++ src/components/views/Feedback/TextAEEditor.jsx
@@ -21,41 +21,44 @@ const TextAEEditor = (props) => {
   /**
    * TextAEEditor의 변경을 감지하여 저장하는 함수
    */
-  const handleBluer = () => {
-    const textContent = JSON.parse(elementRef.current.textContent);
+  const handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === "s") {
+      event.preventDefault(); // 기본 동작(브라우저에서의 저장 다이얼로그 등) 방지
+      const textContent = JSON.parse(elementRef.current.textContent);
 
-    console.log(textContent.denotations); //API를 위한 콘솔 로그
+      console.log(textContent.denotations); //API를 위한 콘솔 로그
 
-    if (textContent.denotations !== "") {
-      let body = {
-        ae_denotations: textContent.denotations,
-        ae_attributes: textContent.attributes,
-      };
-      console.log(body); //API를 위한 콘솔 로그
-      Axios.put(
-        `${API_URL}api/feedback/textae?as_no=${asNo}&user_no=${userNo}`,
-        body,
-        {
-          withCredentials: true,
-        }
-      )
-
-        .then((response) => {
-          if (response.data.isSuccess) {
-            message.success("저장 완료했습니다.");
-            props.setSectioncontent(textContent.denotations);
-            props.setAttributesContent(textContent.attributes);
-            props.setDatacontent(!props.Datacontent);
-          } else {
-            message.error("저장 실패했습니다. 다시 시도해주세요.");
+      if (textContent.denotations !== "") {
+        let body = {
+          ae_denotations: textContent.denotations,
+          ae_attributes: textContent.attributes,
+        };
+        console.log(body); //API를 위한 콘솔 로그
+        Axios.put(
+          `${API_URL}api/feedback/textae?as_no=${asNo}&user_no=${userNo}`,
+          body,
+          {
+            withCredentials: true,
           }
-        })
-        .catch((error) => {
-          // 요청이 실패한 경우의 처리
-          console.log(error);
-          message.error("알 수 없는 에러가 발생했습니다.");
-          navigate("/");
-        });
+        )
+
+          .then((response) => {
+            if (response.data.isSuccess) {
+              message.success("저장 완료했습니다.");
+              props.setSectioncontent(textContent.denotations);
+              props.setAttributesContent(textContent.attributes);
+              props.setDatacontent(!props.Datacontent);
+            } else {
+              message.error("저장 실패했습니다. 다시 시도해주세요.");
+            }
+          })
+          .catch((error) => {
+            // 요청이 실패한 경우의 처리
+            console.log(error);
+            message.error("알 수 없는 에러가 발생했습니다.");
+            navigate("/");
+          });
+      }
     }
   };
 
@@ -102,14 +105,14 @@ const TextAEEditor = (props) => {
   }, [props.Datacontent]);
 
   return (
-    <div>
+    <div onKeyDown={handleKeyDown}>
       <div
         id="my_text-ae_editor"
         className="textae-editor"
         mode="edit"
         inspect="annotation"
-        onBlur={handleBluer}
       ></div>
+
       <div id="annotation" ref={elementRef} style={{ display: "none" }}></div>
     </div>
   );
