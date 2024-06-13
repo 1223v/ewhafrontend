70b58af322ab64ce71fc5fc86f7a20033063d293
hhbb0081 <likesun2000@naver.com>
2024-06-13 21:19:40 +0900

    Feat:메인페이지 배너 추가

---
+++ src/components/views/publishing/MainPage/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./Banner.css";
+
+// 메인페이지 - 배너
+export function Banner() {
+  return (
+    <div className='banner'>
+      {/* <img 
+        src={IMAGES.bannerPC}
+        alt='banner'
+      /> */}
+    </div>
+  )
+}
