fa0e1a3318ba8c927f9a9a1a5ec413ec44887e63
hhbb0081 <likesun2000@naver.com>
2024-06-13 21:27:45 +0900

    Feat: 페이지네이션 커스텀 훅

---
+++ src/hooks/usePagenation.jsx
@@ -0,0 +1,24 @@
+// 페이지네이션 custom hook
+import { useCallback, useEffect, useState } from 'react';
+
+export const usePagenation = (initialLength) => {
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const [pageNumbers, setPageNumbers] = useState([]);
+
+  const calculatePageNumbers = useCallback((len) => {
+    const pages = Array.from(
+      { length: Math.floor(len / 6) + (len % 6 === 0 ? 0 : 1) },
+      (_, i) => i + 1
+    );
+    setPageNumbers(pages);
+  }, []);
+
+  useEffect(() => {
+    calculatePageNumbers(initialLength);
+  }, [initialLength, calculatePageNumbers]);
+
+  const nextPage = () => setCurrentPage((prev) => prev + 1); // 다음 페이지
+  const prevPage = () => setCurrentPage((prev) => prev - 1); // 이전 페이지
+
+  return { nextPage, prevPage, currentPage, setCurrentPage, pageNumbers, calculatePageNumbers };
+}
