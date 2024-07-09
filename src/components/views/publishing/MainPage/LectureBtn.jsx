d5dfea25c02a8e96c3e98afa74b6001e1c8ea0aa
hhbb0081 <hhbb0081@naver.com>
2024-07-09 11:49:01 +0000

    Feat: 학생 수강신청 버튼 추가

--- src/components/views/publishing/MainPage/LectureBtn.jsx
+++ src/components/views/publishing/MainPage/LectureBtn.jsx
@@ -1,14 +1,13 @@
 import { GrAdd } from "react-icons/gr";
 import { useNavigate } from "react-router-dom";
-import { MAIN } from "../../../../constants/text";
 
 // 메인페이지 - 강의 생성 버튼
-export function LectureBtn () {
+export function LectureBtn ({ onClick, children }) {
   const navigate = useNavigate();
   return(
-    <div className="lectureBtn" onClick={() => navigate('/lecture_add')}>
+    <div className="lectureBtn" onClick={onClick}>
       <GrAdd size="15" className="lectureBtn__img"/>
-      <span>{MAIN.BTN.create}</span>
+      <span>{children}</span>
     </div>
   )
 }
