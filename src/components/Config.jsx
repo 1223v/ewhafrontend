b5a63c06d3f8b23ccc8d1b7cce5a8f7bf0679c45
hhbb0081 <likesun2000@naver.com>
2024-07-16 08:48:25 +0000

    Feat: add env url

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -1,3 +1,4 @@
 export const LOGIN_URL = 'https://translation-platform.site:443';
+// export const API_URL = 'https://edu-trans.ewha.ac.kr/';
 export const DOWNLOAD_URL = 'https://edu-trans.ewha.ac.kr/'
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = `${process.env.REACT_APP_API_URL}`;
