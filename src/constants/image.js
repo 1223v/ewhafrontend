b4eee90026b380a9e4117a0a765d5d1c7ca068b5
hhbb0081 <likesun2000@naver.com>
2024-07-20 13:31:56 +0000

    Feat: change logo url

--- src/constants/image.js
+++ src/constants/image.js
@@ -1,6 +1,6 @@
 export const IMAGES = {
   // Nav
-  logo: require("../assets/images/img_logo.png"),
+  logo: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbgyBpv%2FbtsIvbZBan3%2FNuVg84au8x0XpOTF7CMVV0%2Fimg.png",
 
   // 메인페이지
   bannerPC: require("../assets/images/banner_pc.jpg"),
